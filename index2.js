ff0c480b25582df09167715d52c47c2303978df1
AmirAbdolazimi <amirbloody13@gmail.com>
2022-09-17 16:45:57 +0430

    conflict resolved

--- index2.js
+++ index2.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-alert("h111");
-alert("h111");
-alert("h111");
-alert("h111");
-=======
 console.log("h111");
 console.log("h111");
 console.log("h111");
 console.log("h111");
->>>>>>> 78f39709f392bb2116455bb4eaa84640342ed226
